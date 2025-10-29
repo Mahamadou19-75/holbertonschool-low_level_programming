91bc10165f79599234bd28f1b4d7b900bb841e14
Mahamadou19-75 <gadiarramahamadou@gmail.com>
2025-10-29 11:41:49 +0000

    Ajout de la fonction _strlen

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -3,22 +3,16 @@
 
 int _isupper(int c);
 int _isdigit(int c);
-int mul(int a, int b); /* Prototype uniquement */
-/* Prototype de _putchar (souvent déjà fourni) */
+int mul(int a, int b);
 int _putchar(char c);
-/* Prototype de print_numbers */
 void print_numbers(void);
 void print_most_numbers(void);
-/* Prototype de more_numbers */
 void more_numbers(void);
-/* Prototype de print_line (déclaré dans 6-print_line.c ou fichier correspondant) */
 void print_line(int n);
-/* Prototype de print_diagonal (déclaré dans 7-print_diagonal.c) */
 void print_diagonal(int n);
-/* Prototype de print_square (déclaré dans 8-print_square.c) */
 void print_square(int size);
-/* Prototype de print_triangle (déclaré dans 10-print_triangle.c) */
 void print_triangle(int size);
+int _putchar(char c);
 int _strlen(char *s);
 
 #endif
