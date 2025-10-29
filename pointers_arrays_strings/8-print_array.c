da819a9389bf05cdca10acf3e267fce1977afcc8
Mahamadou19-75 <gadiarramahamadou@gmail.com>
2025-10-29 23:46:39 +0000

    Ajout de la fonction 8-print_array pour afficher n éléments d'un tableau

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -14,8 +14,13 @@ void print_array(int *a, int n)
 	{
 	printf("%d", a[i]);
 	if (i != n - 1)
-	printf(", ");
+		printf(", ");
 	}
-	
+	for (i = 0; i < n; i++)
+{
+	printf("%d", a[i]);
+	if (i != n - 1)
+	printf(", ");
+}
 printf("\n");
 }
