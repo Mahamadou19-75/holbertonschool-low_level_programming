867319f20b9b2bc0d86e3511a52b0ac662f621e2
Mahamadou19-75 <gadiarramahamadou@gmail.com>
2025-10-30 00:37:46 +0000

    Correction de 8-main et 8-print_array : style Betty et affichage correct du tableau

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -16,11 +16,6 @@ void print_array(int *a, int n)
 	if (i != n - 1)
 		printf(", ");
 	}
-	for (i = 0; i < n; i++)
-{
-	printf("%d", a[i]);
-	if (i != n - 1)
-	printf(", ");
-}
-printf("\n");
+	
+	printf("\n");
 }
