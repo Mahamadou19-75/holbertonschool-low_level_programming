27d9c1008df7faa282472ba14d1629efc32d4bdf
Mahamadou19-75 <gadiarramahamadou@gmail.com>
2025-11-04 13:46:26 +0000

    tasks 5

--- pointers_arrays_strings/5-main.c
+++ pointers_arrays_strings/5-main.c
@@ -2,16 +2,21 @@
 #include <stdio.h>
 
 /**
- * main - teste la fonction rev_string
+ * main - teste la fonction _strstr
  *
  * Return: toujours 0
  */
 int main(void)
 {
-	char s[10] = "My School";
-    
-    printf("%s\n", s);
-	rev_string(s);
-	printf("%s\n", s);
+	char *s = "hello, world";
+	char *f = "world";
+	char *t;
+
+	/* recherche la sous-chaîne f dans la chaîne s */
+	t = _strstr(s, f);
+
+	/* affiche la partie de s à partir de la sous-chaîne trouvée */
+	printf("%s\n", t);
+
 	return (0);
 }
