20ac174c0cbc02568f445716c6f3b13dd0559d5c
Mahamadou19-75 <gadiarramahamadou@gmail.com>
2025-11-05 15:24:25 +0000

    T4

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -12,7 +12,7 @@ int main(int argc, char *argv[])
 
 	if (argc != 3)
 	{
-        printf("Error\n");
+	printf("Error\n");
 	return (1);
 	}
 
@@ -24,4 +24,3 @@ int main(int argc, char *argv[])
 
 	return (0);
 }
-
